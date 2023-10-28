9b9a27121a86d1cf30d72503fd60290fb6a5c10c
Elizabeth Tucker <elizabethtucker@elizabeths-air.lan>
2023-10-28 15:37:51 -0400

    database restructuring, preliminary switch case coding

--- db/schema.sql
+++ db/schema.sql
@@ -18,3 +18,44 @@ VALUES ("Angela", "Martin", "Head of Accounting", 90000, "Accounting", "N/A"),
 ("Jim","Halpert", "Salesperson", 70000, "Sales", "Dwight Schrute"),
 ("Darryl", "Philbin", "Foreperson", 75000, "Shipping", "N/A"),
 ("Roy", "Anderson", "Shipping Technician", 70000, "Shipping", "Darryl Philbin");
+
+CREATE TABLE department (
+    id INT AUTO_INCREMENT NOT NULL, 
+    dept_name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+)
+
+INSERT INTO department (dept_name)
+VALUES ("Accounting", "Sales", "Shipping")
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    PRIMARY KEY (id)
+)
+
+INSERT INTO role (title, salary)
+VALUES ("Head of Accounting", 90000.00)
+("Accountant", 85000.00)
+("Sales Manager", 75000.00)
+("Salesperson", 72000.00)
+("Foreperson", 75000.00)
+("Shipping Technician", 70000.00)
+
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+
+)
+
+INSERT INTO employee (first_name, last_name)
--- js/server.js
+++ js/server.js
@@ -1,47 +1,55 @@
-const express = require ('express');
-const inquirer = require ('inquirer');
+const express = require('express');
+const inquirer = require('inquirer');
+const mysql = require('mysql')
 
-const app = express ()
+const app = express()
+
+
+const db = mysql.createConnection(
+    {
+      host: 'z3iruaadbwo0iyfp.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+      // MySQL username,
+      user: 'hkjtlht4wjun532d',
+      // MySQL password
+      password: 'yhek9qj986wk3kn7',
+      database: 'rw4bz2y0nt9j6d8j'
+    },
+    console.log(`Connected to the database.`)
+  );
+
 
 const getStarted = [{
     type: 'list',
     message: 'Please make a selection',
     choices: ['View all departments', 'View all roles', 'View all employees', 'Add a Department', 'Add a role', 'Add an employee', 'Update an employee role'],
-  }]
-//   {
-//     type: 'input',
-//     message: 'Please enter a text color:',
-//     name: 'textColor',
-//   },
-//   {
-//     type: 'list',
-//     message: 'Please choose a logo shape:',
-//     name: 'shape',
-//     choices: ['Circle', 'Square', 'Triangle']
-//   },
-//   {
-//     type: 'input',
-//     message: 'Please enter a shape color:',
-//     name: 'shapeColor',
-//   }];
-  
-  
-  inquirer
+}]
+
+inquirer
     .prompt(getStarted)
     .then((response) => {
-      console.log(response)
-    let logoShape;
-          switch (response.shape) {
-          case 'Circle': 
-            logoShape = (new Circle(response.initials, response.textColor, response.shapeColor));
-            break;
-          case 'Square':
-            logoShape = (new Square(response.initials, response.textColor, response.shapeColor));
-            break;
-          case 'Triangle':
-            logoShape = (new Triangle(response.initials, response.textColor, response.shapeColor));
-            break;
-          default:
-            break;
+        console.log(response)
+        let query;
+        switch (response.body) {
+            case 'View all departments':
+                db.query(`SELECT department_name FROM employees`);
+                break;
+            case 'View all roles':
+                db.query(`SELECT title FROM employees`);
+                break;
+            case 'View all employees':
+                db.query(`SELECT first_name last_name FROM employees`);
+            case 'Add a Department':
+                db.query(`INSERT department_name`);
+            case 'Add a role':
+                db.query(`INSERT title`)
+                break;
+            case 'Add an employee':
+                db.query(`INSERT first_name last_name title salary department_name manager`);
+                break;
+            case 'Update an employee role':
+                db.query(`INSERT ?  WHERE id = ?`);
+                break;
+            default:
+                break;
         }
     })
