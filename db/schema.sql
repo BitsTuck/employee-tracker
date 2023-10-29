08afe61bf3d9c405423d63570fbe69b3e9a47672
Elizabeth Tucker <elizabethtucker@elizabeths-air.lan>
2023-10-29 15:14:29 -0400

    schema update and inquirer functionality

--- db/schema.sql
+++ db/schema.sql
@@ -1,43 +1,46 @@
+USE rw4bz2y0nt9j6d8j;
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS department;
+
 CREATE TABLE department (
-    id INT AUTO_INCREMENT NOT NULL, 
+    id INT NOT NULL, 
     dept_name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
-)
+);
 
-INSERT INTO department (dept_name)
-VALUES ("Accounting"),
-("Sales"), 
-("Shipping");
+INSERT INTO department (dept_name, id)
+VALUES ("Accounting", 1),
+("Sales", 2), 
+("Shipping", 3);
 
 CREATE TABLE role (
     id INT AUTO_INCREMENT NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
-    FOREIGN KEY (department_id)
-    REFERENCES department(id)
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id),
     PRIMARY KEY (id)
-)
-
+);
 INSERT INTO role (title, salary)
-VALUES ("Head of Accounting", 90000.00)
-("Accountant", 85000.00)
-("Sales Manager", 75000.00)
-("Salesperson", 72000.00)
-("Foreperson", 75000.00)
-("Shipping Technician", 70000.00)
+VALUES ("Head of Accounting", 90000.00),
+("Accountant", 85000.00),
+("Sales Manager", 75000.00),
+("Salesperson", 72000.00),
+("Foreperson", 75000.00),
+("Shipping Technician", 70000.00);
 
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
+    role_id INT,
     manager_id INT references employee(id),
-    FOREIGN KEY (role_id)
-    REFERENCES role(id)
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    PRIMARY KEY(id)
 
-)
+);
 
 INSERT INTO employee (first_name, last_name)
 VALUES ("Angela", "Martin"),
--- js/server.js
+++ js/server.js
@@ -7,15 +7,15 @@ const app = express()
 
 const db = mysql.createConnection(
     {
-      host: 'z3iruaadbwo0iyfp.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-      // MySQL username,
-      user: 'hkjtlht4wjun532d',
-      // MySQL password
-      password: 'yhek9qj986wk3kn7',
-      database: 'rw4bz2y0nt9j6d8j'
+        host: 'z3iruaadbwo0iyfp.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        // MySQL username,
+        user: 'hkjtlht4wjun532d',
+        // MySQL password
+        password: 'yhek9qj986wk3kn7',
+        database: 'rw4bz2y0nt9j6d8j'
     },
     console.log(`Connected to the database.`)
-  );
+);
 
 
 const getStarted = [{
@@ -24,6 +24,63 @@ const getStarted = [{
     choices: ['View all departments', 'View all roles', 'View all employees', 'Add a Department', 'Add a role', 'Add an employee', 'Update an employee role'],
 }]
 
+const addDept = [{
+    type: 'input',
+    message: 'What is the name of the department?',
+    name: 'name',
+}]
+
+const addRole = [{
+    type: 'input',
+    message: 'What is the name of the role?',
+    name: 'name',
+},
+{
+    type: 'input',
+    message: 'What department does it fall under?',
+    name: 'dept',
+},
+{
+    type: 'input',
+    message: 'What is the salary?',
+    name: 'salary',
+}]
+
+const addEmployee = [{
+    type: 'input',
+    message: 'Employee first name:',
+    name: 'first_name',
+},
+{
+    type: 'input',
+    message: 'Employee last name:',
+    name: 'last_name',
+},
+{
+    type: 'input',
+    message: 'Employee role:',
+    name: 'role',
+},
+{
+    type: 'input',
+    message: 'Manager name:',
+    name: 'manager',
+}]
+
+const updateRole = [{
+    type: 'list',
+    message: 'Which employee do you want to update?',
+    choices: ["Angela Martin", "Oscar Martinez", "Kevin Malone", "Dwight Schrute", "Stanley Hudson", "Jim Halpert", "Darryl Philbin", "Roy Anderson"],
+    name: 'employee',
+},
+{
+    type: 'input',
+    message: 'What is their new role?',
+    name: 'new_update',
+}]
+
+
+
 inquirer
     .prompt(getStarted)
     .then((response) => {
@@ -31,23 +88,43 @@ inquirer
         let query;
         switch (response.choice) {
             case 'View all departments':
-                db.query(`SELECT department_name FROM employees`);
+                db.query('SELECT * FROM department', function (err, results) {
+                    console.log(results);
+                });
                 break;
             case 'View all roles':
-                db.query(`SELECT title FROM employees`);
+                db.query('SELECT * FROM role', function (err, results) {
+                    console.log(results);
+                });
                 break;
             case 'View all employees':
-                db.query(`SELECT first_name last_name FROM employees`);
+                db.query('SELECT * FROM employee', function (err, results) {
+                    console.log(results);
+                });
+                break;
             case 'Add a Department':
-                db.query(`INSERT department_name`);
+                inquirer
+                .prompt(addDept)
+                .then((response) => {
+                db.query(`INSERT ${addDept.name}`);});
+                break;
             case 'Add a role':
-                db.query(`INSERT title`)
+                inquirer
+                .prompt(addRole)
+                .then((response) => {
+                db.query(`INSERT ${addRole.name} ${addRole.dept} ${addRole.salary}`)});
                 break;
             case 'Add an employee':
-                db.query(`INSERT first_name last_name title salary department_name manager`);
+                inquirer
+                .prompt(addEmployee)
+                .then((response) =>{
+                db.query(`INSERT ${addEmployee.first_name} ${addEmployee.last_name} ${addEmployee.role}${addEmployee.manager}`)});
                 break;
             case 'Update an employee role':
-                db.query(`INSERT ?  WHERE id = ?`);
+                inquirer
+                .prompt(updateRole)
+                .then((response) => {
+                db.query(`INSERT ${updateEmployee.new_update} WHERE title = ?`)});
                 break;
             default:
                 break;
