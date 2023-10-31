1258165bc5049de3184255cea9a940b95ff57606
Elizabeth Tucker <elizabethtucker@elizabeths-air.lan>
2023-10-31 08:03:13 -0400

    final schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,4 @@
+
 -- Active: 1698700088044@@127.0.0.1@3306
 USE rw4bz2y0nt9j6d8j;
 DROP TABLE IF EXISTS employee;
@@ -7,46 +8,48 @@ DROP TABLE IF EXISTS department;
 CREATE TABLE department (
     id INT AUTO_INCREMENT, 
     dept_name VARCHAR(30) NOT NULL,
+    manager VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
-INSERT INTO department (dept_name)
-VALUES ("Accounting"),
-("Sales"), 
-("Shipping");
+INSERT INTO department (dept_name, manager)
+VALUES ("Accounting", "Angela Martin"),
+("Sales", "Dwight Schrute"), 
+("Shipping", "Darryl Philbin");
 
 CREATE TABLE role (
     id INT AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT UNSIGNED NOT NULL,
+    dept_name VARCHAR(30) NOT NULL, 
     PRIMARY KEY (id)
 );
-INSERT INTO role (title, salary, department_id)
-VALUES ("Head of Accounting", 90000, 1),
-("Accountant", 85000, 1),
-("Sales Manager", 75000, 2),
-("Salesperson", 72000, 2),
-("Foreperson", 75000, 3),
-("Shipping Technician", 70000, 3);
+INSERT INTO role (title, salary, dept_name)
+VALUES ("Head of Accounting", 90000, "Accounting"),
+("Accountant", 85000, "Accounting"),
+("Sales Manager", 75000, "Sales"),
+("Salesperson", 72000, "Sales"),
+("Foreperson", 75000, "Shipping"),
+("Shipping Technician", 70000, "Shipping");
 
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT,
+    emp_id INT AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT UNSIGNED NOT NULL,
-    manager_id INT,
-    PRIMARY KEY(id)
+    manager VARCHAR (30),
+    PRIMARY KEY(emp_id)
+    );
 
-);
+INSERT INTO employee (first_name, last_name, role_id, manager)
+VALUES ("Angela", "Martin", 1, "N/A"),
+("Oscar", "Martinez", 2, "Angela Martin"),
+("Kevin", "Malone", 2, "Angela Martin"),
+("Dwight", "Schrute", 3, "N/A"),
+("Stanley","Hudson", 4, "Dwight Schrute"),
+("Jim","Halpert", 4, "Dwight Schrute"),
+("Darryl", "Philbin", 5, "N/A"),
+("Roy", "Anderson", 6, "Darryl Philbin");
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Angela", "Martin", 1, 1),
-("Oscar", "Martinez", 2, 1),
-("Kevin", "Malone", 2, 1),
-("Dwight", "Schrute", 2, 2),
-("Stanley","Hudson", 2, 2),
-("Jim","Halpert", 2, 2),
-("Darryl", "Philbin", 3, 3),
-("Roy", "Anderson", 4, 4);
+
